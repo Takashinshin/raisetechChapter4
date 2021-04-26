2f17cc4e6bc0b7422f95fa49fffe4c9bf1325f3d
otatakashi <otatakashi@localhost>
2021-04-26 17:15:38 +0900

    新規プロジェクトデモ用

---
+++ src/main/java/com/example/SpringBootSamplessApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamplessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSamplessApplication.class, args);
+	}
+
+}
